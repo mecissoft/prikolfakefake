5725ed9cd7ed33dac8f4c7346baadebbb96355ba
mecissoft <93204517+mecissoft@users.noreply.github.com>
2021-10-26 18:42:34 +0300

    Тот Самый Файл

---
+++ fakefake.py
@@ -0,0 +1,7 @@
+from pywebio.input import input
+from pywebio.output import put_text
+
+name = input("Как вас зовут")
+skayorim = input("ЛЮБИШЬ СКАЙОРИМ НЕГР ГЕЙ?")
+
+put_text(f"{name} КОТОРЫЙ {skayorim} СКАЙОРИМ НЕГР ГЕЙ ДА ДА ДА ДА ДА ДА ДА ДА ДА ДА ДА ДА ДА ДА!")
